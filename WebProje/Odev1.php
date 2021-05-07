9aa3d639ac9a26f29a85c7c24a8c6edee65d425e
HKPAU <hfbkacar1907@gmail.com>
2021-05-07 16:45:09 +0300

    Web Proje

---
+++ WebProje/Odev1.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="bootstrap-5.0.0-beta3-dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="Odev1.css">
+    <script src="js/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    
+    <div class="MainDiv">
+        <div class="container ConDiv">
+            <table class="LogicTable">
+                <tr>
+                    <td>
+                        <h2 class="HeaderZone">İşitme Kaybı Haber Okumak için Bir Engel Değildir</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td><marquee behavior="" direction=""><h3 class="CEOZone">Hasan Kacar / 17253041 & Oğulcan Doğan / 17253019</h3></marquee></td>
+                </tr>
+                <tr>
+                    <td style="height: 40px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <table>
+                            <tr>
+                                <td><img src="Images/newspaper.png"></td>
+                                <td style="width: 50px;">&nbsp;</td>
+                                <td><img src="Images/deaf.png"></td>
+                                <td style="width: 50px;">&nbsp;</td>
+                                <td><img src="Images/sign-language.png"></td>
+                                <td style="width: 50px;">&nbsp;</td>
+                                <td><img src="Images/multimedia.png"></td>
+                                <td style="width: 50px;">&nbsp;</td>
+                                <td><img src="Images/crossword.png"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height: 40px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                       <div class="card">
+                           <div class="card-header" id="CardHeader">
+                               <img style="width: 750px; height: 320px;" src="Images/Logo.jfif" alt="">
+                           </div>
+                           <div class="card-body" id="CardContent" style="height: 300px;">
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Engelsiz haber ürünü işitme engelli vatandaşlarımızın güncel olarak haber takibi yapmaları için yazılmış
+                                bir haber sitesi.Bu sitenin amacı işitme engeli bulunan vatandaşlarımızın TV ve Gazete haberlerini en
+                                güncel şekilde takip edebilmeleridir.Peki Bu sitenin diğer kanalların kendi sitelerinden veya herhangi bir 
+                                haber sitesinden farkı ne ? Bu site engelli vatandaşların haber programlarını gününde ve saatinde işaret
+                                dili ve altyazı ile takip edebilmelerini sağlıyor.Ayrıca sadece engelli bireyler değil yaşlı veya az da olsa
+                                işitme problemi olan(Kulakları Pek İyi Duymayan :)) bireyler de bu site üzerinden gerek haber takibi yapıp
+                                gerek gazete haberlerini okuyup gerekse bulmaca bile çözebiliyorlar.Bu sitenin diğer yapımlardan bir diğer farkı
+                                da sitenin hem güncel haberleri yayınlaması hem de bu güncel haberleri işaret dili ve extradan altyazı ile birlikte yayınlaması.
+                                Ayrıca sadece kanallardaki haberler değil tüm medya haberleri ve gazate haberleri de bu site de mevcut.
+                           </div>
+                           <div class="card-footer text-muted" id="CardFooter">Hasan Kacar / Oğulcan Doğan</div>
+                       </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+
+    <script src=bootstrap-5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+</body>
+</html>
